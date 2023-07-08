13c7e51fbdbf91e7153341d9301f8de0c7931a1d
Enrique Ruibal A <enriqueruibal@hotmail.com>
2023-07-08 15:06:03 -0700

    CE 7. Equal Sum Checker

---
+++ EqualSummChecker.java
@@ -0,0 +1,7 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum (int numone, int numtwo, int numthree) {
+        if (numone+numtwo == numthree) {
+            return true;
+        } else return false;
+    }
+}
