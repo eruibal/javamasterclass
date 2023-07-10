3dc3876b633a5e609ecf8e214158e5069a7b7563
Enrique Ruibal A <enriqueruibal@hotmail.com>
2023-07-09 19:07:47 -0700

    58. Switch Challenge

---
+++ SwitchChallenge/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        char letter = 'E';
+        switch (letter) {
+            case 'A':
+                System.out.println(letter+" Able");
+                break;
+            case 'B':
+                System.out.println(letter+" Baker");
+                break;
+            case 'C':
+                System.out.println(letter+" Charlie");
+                break;
+            case 'D':
+                System.out.println(letter+" Dog");
+                break;
+            case 'E':
+                System.out.println(letter+" Easy");
+                break;
+            default:
+                System.out.println("Not found");
+        }
+    }
+}
